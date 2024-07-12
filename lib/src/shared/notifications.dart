1756bc4d1fc39e41cef2c7d7bb07d1cf092272aa
Mazen Omar <wildlifemain1@gmail.com>
2024-07-12 16:39:32 +0300

    fix ios

--- lib/src/shared/notifications.dart
+++ lib/src/shared/notifications.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:google_api_availability/google_api_availability.dart';
@@ -7,10 +8,16 @@ class FirebaseNotifications {
   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
 
   init() async {
+    if(Platform.isIOS){
+      await _messaging.requestPermission();
+      final token = await getToken();
+      log("Token $token");
+    }
     GooglePlayServicesAvailability availability = await GoogleApiAvailability
         .instance
         .checkGooglePlayServicesAvailability();
       log("[Google Play Services] $availability");
+    
     if (availability == GooglePlayServicesAvailability.success) {
       await _messaging.requestPermission();
       final token = await getToken();
